6c0978e5191d9491a5b7b7646a5a2c1bd401622d
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2026-08-27 22:33:53 +0200

    Added Endian Reversal

--- main.c
+++ main.c
@@ -1,14 +1,32 @@
 #include "stdio.h"
 
+void bytes_reverser(char* first_byte, size_t total_bytes){
+    char temp;
+    for(int i=0; i<total_bytes/2; i++){
+        temp = first_byte[i];
+        first_byte[i] = first_byte[total_bytes - i -1];
+        first_byte[total_bytes - i - 1] = temp;
+    }
+    return;
+}
+
 int main(){
-    int value = 0x12345601;
-    
+    int value = 0x78563412;     // Pretending a Big Endian System
     char *bytes = (char *)&value;
 
-    if(bytes[0] == 1)
+    if(bytes[0] == 0x78)
         printf("Little Endian System\n");
-    else
+    else{
         printf("Big Endian System\n");
+        // Before Reversal
+        printf("0x%08X\n", value);
+
+        bytes_reverser(bytes, sizeof(value));
+
+        // After Reversal
+        printf("0x%08X\n", value);
+
+    }
 
         return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
 #include <cstring>
+#include <iomanip>
 
 union EndianCheck {
     int value;
     char bytes[4];
 };
 
+void bytes_reverser(char* first_byte, size_t total_bytes){
+    char temp;
+    for(int i=0; i<total_bytes/2; i++){
+        temp = first_byte[i];
+        first_byte[i] = first_byte[total_bytes - i -1];
+        first_byte[total_bytes - i - 1] = temp;
+    }
+    return;
+}
+
 int main(){
-    int value = 1;
+    int value = 0x78563412;     // Pretending a Big Endian System
     char byte;
     memcpy(&byte, &value, 1);
 
     EndianCheck endian;
-    endian.value = 1;
-    std::cout << static_cast<int>(endian.bytes[0]) << "\n";
+    endian.value = value;
 
-    if(byte == 1)
+    if(byte == 0x78 && endian.bytes[0] == 0x78)
         std::cout << "Little Endian System\n";
-    else
+    else{
         std::cout << "Big Endian System\n";
+
+        // Before Reversal
+        std::cout << std::hex
+              << "0x" 
+              << std::uppercase 
+              << std::setw(8) 
+              << std::setfill('0') 
+              << value << std::endl
+              << std::dec << std::nouppercase;
+
+        bytes_reverser((char*)&value, sizeof(value));
+
+        // After Reversal
+        std::cout << std::hex
+              << "0x" 
+              << std::uppercase 
+              << std::setw(8) 
+              << std::setfill('0') 
+              << value << std::endl
+              << std::dec << std::nouppercase; 
+    }
 
     return 0;
 }
